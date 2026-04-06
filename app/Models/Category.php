99b12fc6100eedce60e186a630498811e51f0b43
vitanh1703 <diema448@gmail.com>
2026-04-06 13:50:17 +0700

    update model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'sv23810310083_categories';
+
+    protected $fillable = ['name', 'slug', 'description', 'is_visible'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
 }
