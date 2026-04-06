d9bb486301b646f5c3e12f45ae74108f601f9040
vitanh1703 <diema448@gmail.com>
2026-04-06 13:48:50 +0700

    update model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $table = 'sv23810310083_products';
+
+    protected $fillable = [
+        'category_id', 'name', 'slug', 'description', 
+        'price', 'stock_quantity', 'image_path', 'status', 'warranty_months'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
